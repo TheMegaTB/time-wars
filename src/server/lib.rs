8ceb3a76c1e45bfcd3fcf85b8d1f7f18306b15f3
Til Blechschmidt <til@blechschmidt.de>
2016-01-17 14:22:26 +0100

    Added create_portal function

--- src/server/lib.rs
+++ src/server/lib.rs
@@ -61,6 +61,31 @@ impl Server {
         }
     }
 
+    fn create_portal(&mut self, player: Player,
+                origin: (TimeIndex, Coordinates), origin_lifetime: TimeIndex, origin_scale: f32,
+                dest: (TimeIndex, Coordinates), dest_lifetime: TimeIndex, dest_scale: f32) {
+        let origin = Endpoint {
+            location: origin.1,
+            creation: origin.0,
+            expiration: origin.0 + origin_lifetime,
+            scale: origin_scale
+        };
+        let dest = Endpoint {
+            location: dest.1,
+            creation: dest.0,
+            expiration: dest.0 + dest_lifetime,
+            scale: dest_scale
+        };
+        self.portals.push(
+            Portal {
+                player: player,
+                origin: origin,
+                dest: dest,
+                compression_factor: (dest_scale / origin_scale, (dest_lifetime as f32) / (origin_lifetime as f32))
+            }
+        )
+    }
+
     fn get_ai(&self, id: ID) -> &AI {
         &self.ais[id]
     }
@@ -106,8 +131,22 @@ impl Server {
         }
     }
 
+    fn print_portal(&self, id: usize) {
+        let ref p = self.portals[id];
+        let ref origin = p.origin;
+        let ref dest = p.dest;
+        println!("ORIGIN: [X: {}, Y: {}, T: {}], Expiration: {}",
+                    origin.location.0, origin.location.1, origin.creation, origin.expiration);
+        println!("DESTIN: [X: {}, Y: {}, T: {}], Expiration: {}",
+                    dest.location.0, dest.location.1, dest.creation, dest.expiration);
+        println!("Compression ratio (size): {}", p.compression_factor.0);
+        println!("Compression ratio (time): {}", p.compression_factor.1);
+    }
+
     pub fn start_game(&mut self) {
         println!("A new game has been started!");
+        self.create_portal(0, (100, (1.0, 1.0)), 500, 1.0, (0, (2.0, 2.0)), 100, 4.0);
+        self.print_portal(0);
     }
 }
 
@@ -133,4 +172,12 @@ fn calculate_keyframes() {
         let loc = s.keyframes.get(&(20 as usize)).unwrap()[0].1;
         assert_eq!(loc, (20.0, 400.0));
     }
+}
+
+#[test]
+fn compression_ratio() {
+    let mut s = Server::new();
+    s.create_portal(0, (100, (1.0, 1.0)), 500, 1.0, (0, (2.0, 2.0)), 100, 4.0);
+    assert_eq!(s.portals[0].compression_factor.0, 4f32);
+    assert_eq!(s.portals[0].compression_factor.1, 0.2f32);
 }
